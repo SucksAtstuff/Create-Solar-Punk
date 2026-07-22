c7e16f5650fdab15621c4fb35524d541e90d3775
succ <Kalle170512@gmail.com>
2026-07-22 13:56:16 +0200

    Fixed turbine blades always facing same direction regardless of placement

--- src/main/java/net/succ/solar_punk/block/custom/BrassTurbineBladeBlock.java
+++ src/main/java/net/succ/solar_punk/block/custom/BrassTurbineBladeBlock.java
@@ -1,22 +1,61 @@
 package net.succ.solar_punk.block.custom;
 
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class BrassTurbineBladeBlock extends Block implements IWrenchable {
 
-    public static final BooleanProperty HIDDEN = BooleanProperty.create("hidden");
+    public static final BooleanProperty HIDDEN  = BooleanProperty.create("hidden");
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    private static final VoxelShape SHAPE_EAST_WEST   = Block.box(0, 0, 0, 16, 2, 16);
+    private static final VoxelShape SHAPE_NORTH_SOUTH = Block.box(0, 0, 0, 16, 2, 16);
 
     public BrassTurbineBladeBlock(Properties properties) {
         super(properties);
-        registerDefaultState(defaultBlockState().setValue(HIDDEN, false));
+        registerDefaultState(defaultBlockState()
+                .setValue(FACING, Direction.EAST)
+                .setValue(HIDDEN, false));
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(HIDDEN);
+        builder.add(FACING, HIDDEN);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection())
+                .setValue(HIDDEN, false);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        Direction facing = state.getValue(FACING);
+        return (facing == Direction.NORTH || facing == Direction.SOUTH) ? SHAPE_NORTH_SOUTH : SHAPE_EAST_WEST;
     }
 }
